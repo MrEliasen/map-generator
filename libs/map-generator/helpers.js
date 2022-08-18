a3665becce36fe0d6a614a9ce3aa85e34a6ceebb
Mark Eliasen <mark.eliasen@zolidsolutions.com>
2022-08-18 21:17:47 +0100

    move convert grid helpers into map gen helpers

--- libs/map-generator/helpers.js
+++ libs/map-generator/helpers.js
@@ -207,3 +207,23 @@ export function getOppositeAxisDirections(direction) {
         return false;
     });
 }
+
+export function convertFromScanline(scanlineGrid) {
+    const grid = [...Array(scanlineGrid[0].length)].map(() => []);
+
+    scanlineGrid.forEach((y) => y.forEach((x, index) => {
+        grid[index].push(x);
+    }));
+
+    return grid;
+}
+
+export function convertToScanline(grid) {
+    const scanlineGrid = [...Array(grid.length)].map(() => []);
+
+    grid.forEach((x) => x.forEach((y, index) => {
+        scanlineGrid[index].push(y);
+    }));
+
+    return scanlineGrid;
+}
